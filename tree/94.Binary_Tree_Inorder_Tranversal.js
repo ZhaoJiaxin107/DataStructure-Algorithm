0f065e343a93ac20b602efa325884a8a7d74e37f
Jiaxin Zhao <jzhao537@uwo.ca>
2020-12-01 00:16:31 +0800

    Create 94 binary tree inorder tranversal

---
+++ tree/94.Binary_Tree_Inorder_Tranversal.js
@@ -0,0 +1,46 @@
+/* Given the root of a binary tree, return the inorder traversal 
+of its nodes' values.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res = [];
+    const rec = (n) =>{
+        if(!n) return;
+        rec(n.left);
+        res.push(n.val);
+        rec(n.right);
+    };
+    rec(root);
+    return res;
+};
+
+var inorderTraversal = function(root) {
+    const res = [];
+    const stack = [];
+    let p = root;
+    while(stack.length || p){
+        while(p){
+            stack.push(p);
+            p = p.left;
+        }
+        const n = stack.pop();
+        res.push(n.val);
+        p = n.right;
+    }
+    return res;
+};
+
+// 时间复杂度: O(n) n为所有节点
+// 空间复杂度: O(n) n为所有节点
